b5af1d04ae215768976299bbcf28ccd407cb0acf
romasmi <rsmarsu@gmail.com>
2025-11-09 22:08:19 +0700

    Add util for JSON response.

--- internal/utils/http.go
+++ internal/utils/http.go
@@ -1,7 +1,26 @@
 package utils
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func ErrorInvalidRequestBody(w http.ResponseWriter) {
-	http.Error(w, "invalid request body", http.StatusBadRequest)
+func ErrorInvalidRequestBody(w http.ResponseWriter, err error) {
+	http.Error(w, fmt.Sprintf("invalid request body, %s", err.Error()), http.StatusBadRequest)
+}
+
+func JsonResponse(w http.ResponseWriter, output interface{}) {
+	err := json.NewEncoder(w).Encode(output)
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		fmt.Printf("error while encoding response: %v\n", err)
+		JsonError(w, fmt.Errorf("internal error"))
+	}
+}
+
+func JsonError(w http.ResponseWriter, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	_ = json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 }
