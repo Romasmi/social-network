ef042a7327f683ca3fed8e73207f089b0a8884b5
romasmi <rsmarsu@gmail.com>
2025-11-09 21:35:22 +0700

    Remove updateAt for user model.

--- internal/models/user.go
+++ internal/models/user.go
@@ -11,7 +11,6 @@ type User struct {
 	Email        string    `json:"email"`
 	IsActive     bool      `json:"isActive"`
 	PasswordHash string    `json:"-"`
-	UpdateAt     time.Time `json:"updatedAt"`
 	CreatedAt    time.Time `json:"createdAt"`
 }
 
