c31d4c1e123bb2839ac2497de6507b77307c9dd8
romasmi <rsmarsu@gmail.com>
2025-12-01 01:07:00 +0700

    More user token for load testing to ENV var.

--- load_testing/user_search.js
+++ load_testing/user_search.js
@@ -2,18 +2,13 @@ import http from "k6/http";
 import { sleep } from "k6";
 import { check } from "k6";
 
-export let options = {
-    vus: 1, // 1, 10, 100, 1000
-    duration: "30s",
-};
-
 export default function () {
     const url = "http://localhost:8000/user/search?first_name=%D0%90%D0%B1%D1%80%D0%B0&last_name=%D0%A2%D0%B8%D0%BC";
 
     const params = {
         headers: {
             Accept: "application/json",
-            Authorization: "Bearer 019ad5a1-bee6-7271-8127-c14f2279b3fa",
+            Authorization: "Bearer " + __ENV.TOKEN,
         },
     };
 
