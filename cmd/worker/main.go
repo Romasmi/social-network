f2a90b3b732804a838dd81d6599cef7423c50529
romasmi <rsmarsu@gmail.com>
2025-12-30 01:13:46 +0700

    Add worker endpoint.

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,47 @@
 package worker
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Romasmi/social-network/internal/app"
+)
+
+func main() {
+	basePath := os.Getenv("APP_BASE_PATH")
+	if basePath == "" {
+		basePath = "." // current directory
+	}
+
+	appInstance, err := app.NewApp(basePath)
+	if err != nil {
+		fmt.Printf("error while app init: %v", err)
+		os.Exit(1)
+	}
+	worker := app.NewApi(appInstance)
+
+	go func() {
+		if err := worker.Run(); err != nil {
+			fmt.Printf("server error: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	fmt.Println("Shutting down gracefully...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := worker.Shutdown(ctx); err != nil {
+		fmt.Printf("Error during shutdown: %v\n", err)
+	}
+
+	fmt.Println("Server stopped")
+}
